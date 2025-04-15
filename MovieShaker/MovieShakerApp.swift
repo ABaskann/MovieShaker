5781dc400341035bf351139144c538419d6be011
ABaskann <armagan.baskann@gmail.com>
2025-04-15 15:07:55 +0300

    Initial Commit

---
+++ MovieShaker/MovieShakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieShakerApp.swift
+//  MovieShaker
+//
+//  Created by Armağan Başkan on 15.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieShakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
